14a2d3b7786b6a11e79fe30bb41f8fb98a69dc24
JFWakka <jfarzanfar1@gmail.com>
2015-03-22 14:39:40 -0400

    fhg

--- ScriptFile1.R
+++ ScriptFile1.R
@@ -1,18 +1,20 @@
-# TODO: Add comment
-# 
-# Author: Jonny
-###############################################################################
+## TODO: Add comment
+## 
+## Author: Jonny
+##############################################################################################################################################################
 
 library(arules)
 
-memory.size(20000)
+##memory.size(20000)
 
 
 f <- file("http://archive.ics.uci.edu/ml/machine-learning-databases/mushroom/agaricus-lepiota.data")
 
 data <- read.table(f, sep=",", header=F)
 
-#summary(data)
+##data <- read.dcf("C:\Python27\agaricus-lepiota.data")
+
+summary(data)
 
 cnames <- list("EatIt","CapShape","Cap-Surface","cap-color","bruises","odor","gill-attachment","gill-spacing","gill-size","gill-color",
 		"stalk-shape","stalk-root","stalk-surface-above-ring","stalk-surface-below-ring","stalk-color-above-ring","stalk-color-below-ring"
@@ -20,28 +22,28 @@ cnames <- list("EatIt","CapShape","Cap-Surface","cap-color","bruises","odor","gi
 
 colnames(data) <- cnames
 
-#eatornot
+##eatornot
 levels(data$EatIt)[match("p",levels(data$EatIt))] <- "poisonous"
 levels(data$EatIt)[match("e",levels(data$EatIt))] <- "edible"
-#data$EatIt
+##data$EatIt
 
-#CapShape
+##CapShape
 levels(data$CapShape)[match("b",levels(data$CapShape))] <- "bell"
 levels(data$CapShape)[match("c",levels(data$CapShape))] <- "conical"
 levels(data$CapShape)[match("x",levels(data$CapShape))] <- "convex"
 levels(data$CapShape)[match("f",levels(data$CapShape))] <- "flat"
 levels(data$CapShape)[match("k",levels(data$CapShape))] <- "knobbed"
 levels(data$CapShape)[match("s",levels(data$CapShape))] <- "sunken"
-#data$CapShape
+##data$CapShape
 
-#cap-surface:              fibrous=f,grooves=g,scaly=y,smooth=s
+##cap-surface:              fibrous=f,grooves=g,scaly=y,smooth=s
 levels(data$`Cap-Surface`)[match("s",levels(data$`Cap-Surface`))] <- "smooth"
 levels(data$`Cap-Surface`)[match("f",levels(data$`Cap-Surface`))] <- "fibrous"
 levels(data$`Cap-Surface`)[match("g",levels(data$`Cap-Surface`))] <- "grooves"
 levels(data$`Cap-Surface`)[match("y",levels(data$`Cap-Surface`))] <- "scaly"
-#data$`Cap-Surface`
+##data$`Cap-Surface`
 
-#cap-color:                brown=n,buff=b,cinnamon=c,gray=g,green=r,pink=p,purple=u,red=e,white=w,yellow=y
+##cap-color:                brown=n,buff=b,cinnamon=c,gray=g,green=r,pink=p,purple=u,red=e,white=w,yellow=y
 levels(data$`cap-color`)[match("n",levels(data$`cap-color`))] <- "brown"
 levels(data$`cap-color`)[match("b",levels(data$`cap-color`))] <- "buff"
 levels(data$`cap-color`)[match("c",levels(data$`cap-color`))] <- "cinnamon"
@@ -52,14 +54,14 @@ levels(data$`cap-color`)[match("u",levels(data$`cap-color`))] <- "purple"
 levels(data$`cap-color`)[match("e",levels(data$`cap-color`))] <- "red"
 levels(data$`cap-color`)[match("w",levels(data$`cap-color`))] <- "white"
 levels(data$`cap-color`)[match("y",levels(data$`cap-color`))] <- "yellow"
-#data$`cap-color`
+##data$`cap-color`
 
-#bruises?:                 bruises=t,no=f
+##bruises?:                 bruises=t,no=f
 levels(data$bruises)[match("t",levels(data$bruises))] <- "bruises"
 levels(data$bruises)[match("f",levels(data$bruises))] <- "no_bruises"
-#data$bruises
+##data$bruises
 
-#odor:                     almond=a,anise=l,creosote=c,fishy=y,foul=f,musty=m,none=n,pungent=p,spicy=s
+##odor:                     almond=a,anise=l,creosote=c,fishy=y,foul=f,musty=m,none=n,pungent=p,spicy=s
 levels(data$odor)[match("a",levels(data$odor))] <- "almond"
 levels(data$odor)[match("l",levels(data$odor))] <- "anise"
 levels(data$odor)[match("c",levels(data$odor))] <- "creosote"
@@ -69,27 +71,27 @@ levels(data$odor)[match("m",levels(data$odor))] <- "musty"
 levels(data$odor)[match("n",levels(data$odor))] <- "none"
 levels(data$odor)[match("p",levels(data$odor))] <- "pungent"
 levels(data$odor)[match("s",levels(data$odor))] <- "spicy"
-#data$odor
+##data$odor
 
-#gill-attachment:          attached=a,descending=d,free=f,notched=n
+##gill-attachment:          attached=a,descending=d,free=f,notched=n
 levels(data$`gill-attachment`)[match("a",levels(data$`gill-attachment`))] <- "attached"
 levels(data$`gill-attachment`)[match("d",levels(data$`gill-attachment`))] <- "descending"
 levels(data$`gill-attachment`)[match("f",levels(data$`gill-attachment`))] <- "free"
 levels(data$`gill-attachment`)[match("n",levels(data$`gill-attachment`))] <- "notched"
-#data$`gill-attachment`
+##data$`gill-attachment`
 
-#gill-spacing:             close=c,crowded=w,distant=d
+##gill-spacing:             close=c,crowded=w,distant=d
 levels(data$`gill-spacing`)[match("c",levels(data$`gill-spacing`))] <- "close"
 levels(data$`gill-spacing`)[match("w",levels(data$`gill-spacing`))] <- "crowded"
 levels(data$`gill-spacing`)[match("d",levels(data$`gill-spacing`))] <- "distant"
-#data$`gill-spacing`
+##data$`gill-spacing`
 
-#gill-size:                broad=b,narrow=n
+##gill-size:                broad=b,narrow=n
 levels(data$`gill-size`)[match("b",levels(data$`gill-size`))] <- "broad"
 levels(data$`gill-size`)[match("n",levels(data$`gill-size`))] <- "narrow"
-#data$`gill-size`
+##data$`gill-size`
 
-#gill-color:               black=k,brown=n,buff=b,chocolate=h,gray=g,green=r,orange=o,pink=p,purple=u,red=e,white=w,yellow=y
+##gill-color:               black=k,brown=n,buff=b,chocolate=h,gray=g,green=r,orange=o,pink=p,purple=u,red=e,white=w,yellow=y
 levels(data$`gill-color`)[match("k",levels(data$`gill-color`))] <- "black"
 levels(data$`gill-color`)[match("n",levels(data$`gill-color`))] <- "brown"
 levels(data$`gill-color`)[match("b",levels(data$`gill-color`))] <- "buff"
@@ -102,14 +104,14 @@ levels(data$`gill-color`)[match("u",levels(data$`gill-color`))] <- "purple"
 levels(data$`gill-color`)[match("e",levels(data$`gill-color`))] <- "red"
 levels(data$`gill-color`)[match("w",levels(data$`gill-color`))] <- "white"
 levels(data$`gill-color`)[match("y",levels(data$`gill-color`))] <- "yellow"
-#data$`gill-color`
+##data$`gill-color`
 
-#stalk-shape:              enlarging=e,tapering=t
+##stalk-shape:              enlarging=e,tapering=t
 levels(data$`stalk-shape`)[match("e",levels(data$`stalk-shape`))] <- "enlarging"
 levels(data$`stalk-shape`)[match("t",levels(data$`stalk-shape`))] <- "tapering"
-#data$`stalk-shape`
+##data$`stalk-shape`
 
-#stalk-root:               bulbous=b,club=c,cup=u,equal=e,rhizomorphs=z,rooted=r,missing=?
+##stalk-root:               bulbous=b,club=c,cup=u,equal=e,rhizomorphs=z,rooted=r,missing=?
 levels(data$`stalk-root`)[match("b",levels(data$`stalk-root`))] <- "bulbous"
 levels(data$`stalk-root`)[match("c",levels(data$`stalk-root`))] <- "club"
 levels(data$`stalk-root`)[match("u",levels(data$`stalk-root`))] <- "cup"
@@ -117,23 +119,23 @@ levels(data$`stalk-root`)[match("e",levels(data$`stalk-root`))] <- "equal"
 levels(data$`stalk-root`)[match("z",levels(data$`stalk-root`))] <- "rhizomorphs"
 levels(data$`stalk-root`)[match("r",levels(data$`stalk-root`))] <- "rooted"
 levels(data$`stalk-root`)[match("?",levels(data$`stalk-root`))] <- "missing"
-#data$`stalk-root`
+##data$`stalk-root`
 
-#stalk-surface-above-ring: fibrous=f,scaly=y,silky=k,smooth=s
+##stalk-surface-above-ring: fibrous=f,scaly=y,silky=k,smooth=s
 levels(data$`stalk-surface-above-ring`)[match("f",levels(data$`stalk-surface-above-ring`))] <- "fibrous"
 levels(data$`stalk-surface-above-ring`)[match("y",levels(data$`stalk-surface-above-ring`))] <- "scaly"
 levels(data$`stalk-surface-above-ring`)[match("k",levels(data$`stalk-surface-above-ring`))] <- "silky"
 levels(data$`stalk-surface-above-ring`)[match("s",levels(data$`stalk-surface-above-ring`))] <- "smooth"
-#data$`stalk-surface-above-ring`
+##data$`stalk-surface-above-ring`
 
-#stalk-surface-below-ring: fibrous=f,scaly=y,silky=k,smooth=s
+##stalk-surface-below-ring: fibrous=f,scaly=y,silky=k,smooth=s
 levels(data$`stalk-surface-below-ring`)[match("f",levels(data$`stalk-surface-below-ring`))] <- "fibrous"
 levels(data$`stalk-surface-below-ring`)[match("y",levels(data$`stalk-surface-below-ring`))] <- "scaly"
 levels(data$`stalk-surface-below-ring`)[match("k",levels(data$`stalk-surface-below-ring`))] <- "silky"
 levels(data$`stalk-surface-below-ring`)[match("s",levels(data$`stalk-surface-below-ring`))] <- "smooth"
-#data$`stalk-surface-below-ring`
+##data$`stalk-surface-below-ring`
 
-#stalk-color-above-ring:   brown=n,buff=b,cinnamon=c,gray=g,orange=o,pink=p,red=e,white=w,yellow=y
+##stalk-color-above-ring:   brown=n,buff=b,cinnamon=c,gray=g,orange=o,pink=p,red=e,white=w,yellow=y
 levels(data$`stalk-color-above-ring`)[match("b",levels(data$`stalk-color-above-ring`))] <- "buff"
 levels(data$`stalk-color-above-ring`)[match("n",levels(data$`stalk-color-above-ring`))] <- "brown"
 levels(data$`stalk-color-above-ring`)[match("g",levels(data$`stalk-color-above-ring`))] <- "gray"
@@ -143,9 +145,9 @@ levels(data$`stalk-color-above-ring`)[match("c",levels(data$`stalk-color-above-r
 levels(data$`stalk-color-above-ring`)[match("e",levels(data$`stalk-color-above-ring`))] <- "red"
 levels(data$`stalk-color-above-ring`)[match("w",levels(data$`stalk-color-above-ring`))] <- "white"
 levels(data$`stalk-color-above-ring`)[match("y",levels(data$`stalk-color-above-ring`))] <- "yellow"
-#data$`stalk-color-above-ring`
+##data$`stalk-color-above-ring`
 
-#stalk-color-below-ring:   brown=n,buff=b,cinnamon=c,gray=g,orange=o,pink=p,red=e,white=w,yellow=y
+##stalk-color-below-ring:   brown=n,buff=b,cinnamon=c,gray=g,orange=o,pink=p,red=e,white=w,yellow=y
 levels(data$`stalk-color-below-ring`)[match("b",levels(data$`stalk-color-below-ring`))] <- "buff"
 levels(data$`stalk-color-below-ring`)[match("n",levels(data$`stalk-color-below-ring`))] <- "brown"
 levels(data$`stalk-color-below-ring`)[match("g",levels(data$`stalk-color-below-ring`))] <- "gray"
@@ -155,29 +157,29 @@ levels(data$`stalk-color-below-ring`)[match("c",levels(data$`stalk-color-below-r
 levels(data$`stalk-color-below-ring`)[match("e",levels(data$`stalk-color-below-ring`))] <- "red"
 levels(data$`stalk-color-below-ring`)[match("w",levels(data$`stalk-color-below-ring`))] <- "white"
 levels(data$`stalk-color-below-ring`)[match("y",levels(data$`stalk-color-below-ring`))] <- "yellow"
-#data$`stalk-color-below-ring`
+##data$`stalk-color-below-ring`
 
 
-#veil-type:                partial=p,universal=u
+##veil-type:                partial=p,universal=u
 levels(data$`veil-type`)[match("p",levels(data$`veil-type`))] <- "partial"
 levels(data$`veil-type`)[match("u",levels(data$`veil-type`))] <- "universal"
-#data$`veil-type`
+##data$`veil-type`
 
-#veil-color:               brown=n,orange=o,white=w,yellow=y
+##veil-color:               brown=n,orange=o,white=w,yellow=y
 levels(data$`veil-color`)[match("w",levels(data$`veil-color`))] <- "white"
 levels(data$`veil-color`)[match("y",levels(data$`veil-color`))] <- "yellow"
 levels(data$`veil-color`)[match("o",levels(data$`veil-color`))] <- "orange"
 levels(data$`veil-color`)[match("n",levels(data$`veil-color`))] <- "brown"
-#data$`veil-color`
+##data$`veil-color`
 
-#ring-number:              none=n,one=o,two=t
+##ring-number:              none=n,one=o,two=t
 levels(data$`ring-number`)[match("n",levels(data$`ring-number`))] <- "none"
 levels(data$`ring-number`)[match("t",levels(data$`ring-number`))] <- "two"
 levels(data$`ring-number`)[match("o",levels(data$`ring-number`))] <- "one"
-#data$`ring-number`
+##data$`ring-number`
 
 
-#ring-type:                cobwebby=c,evanescent=e,flaring=f,large=l,none=n,pendant=p,sheathing=s,zone=z
+##ring-type:                cobwebby=c,evanescent=e,flaring=f,large=l,none=n,pendant=p,sheathing=s,zone=z
 levels(data$`ring-type`)[match("c",levels(data$`ring-type`))] <- "cobwebby"
 levels(data$`ring-type`)[match("e",levels(data$`ring-type`))] <- "evanescent"
 levels(data$`ring-type`)[match("f",levels(data$`ring-type`))] <- "flaring"
@@ -186,9 +188,9 @@ levels(data$`ring-type`)[match("n",levels(data$`ring-type`))] <- "none"
 levels(data$`ring-type`)[match("p",levels(data$`ring-type`))] <- "pendant"
 levels(data$`ring-type`)[match("s",levels(data$`ring-type`))] <- "sheathing"
 levels(data$`ring-type`)[match("z",levels(data$`ring-type`))] <- "zone"
-#data$`ring-type`
+##data$`ring-type`
 
-#spore-print-color:        black=k,brown=n,buff=b,chocolate=h,green=r,orange=o,purple=u,white=w,yellow=y
+##spore-print-color:        black=k,brown=n,buff=b,chocolate=h,green=r,orange=o,purple=u,white=w,yellow=y
 levels(data$`spore-print-color`)[match("k",levels(data$`spore-print-color`))] <- "black"
 levels(data$`spore-print-color`)[match("n",levels(data$`spore-print-color`))] <- "brown"
 levels(data$`spore-print-color`)[match("b",levels(data$`spore-print-color`))] <- "buff"
@@ -201,19 +203,19 @@ levels(data$`spore-print-color`)[match("u",levels(data$`spore-print-color`))] <-
 levels(data$`spore-print-color`)[match("e",levels(data$`spore-print-color`))] <- "red"
 levels(data$`spore-print-color`)[match("w",levels(data$`spore-print-color`))] <- "white"
 levels(data$`spore-print-color`)[match("y",levels(data$`spore-print-color`))] <- "yellow"
-#data$`spore-print-color`
+##data$`spore-print-color`
 
-#population:               abundant=a,clustered=c,numerous=n,scattered=s,several=v,solitary=y
+##population:               abundant=a,clustered=c,numerous=n,scattered=s,several=v,solitary=y
 levels(data$population)[match("a",levels(data$population))] <- "abundant"
 levels(data$population)[match("c",levels(data$population))] <- "clustered"
 levels(data$population)[match("n",levels(data$population))] <- "numerous"
 levels(data$population)[match("s",levels(data$population))] <- "scattered"
 levels(data$population)[match("v",levels(data$population))] <- "serveral"
 levels(data$population)[match("y",levels(data$population))] <- "solitary"
-#data$population
+##data$population
 
 
-#habitat:                  grasses=g,leaves=l,meadows=m,paths=p,urban=u,waste=w,woods=d
+##habitat:                  grasses=g,leaves=l,meadows=m,paths=p,urban=u,waste=w,woods=d
 levels(data$habitat)[match("g",levels(data$habitat))] <- "grasses"
 levels(data$habitat)[match("l",levels(data$habitat))] <- "leaves"
 levels(data$habitat)[match("m",levels(data$habitat))] <- "meadows"
@@ -221,16 +223,29 @@ levels(data$habitat)[match("p",levels(data$habitat))] <- "paths"
 levels(data$habitat)[match("u",levels(data$habitat))] <- "urban"
 levels(data$habitat)[match("w",levels(data$habitat))] <- "waste"
 levels(data$habitat)[match("d",levels(data$habitat))] <- "woods"
-#data$habitat
+##data$habitat
 
-rules <- apriori(data,control = list(verbose=F),
-		parameter = list(minlen=2, supp=0.005, conf=0.8), 
-		appearance = list(rhs=c("EatIt=edible","EatIt=poisonous"),
-				default="lhs"))
+##data
 
+##data2 <- subset(data, EatIt == "poisonous")
+##data2[1:10,]
+
+##data2 <- subset(data, EatIt == "poisonous")
+
+##levels(data2) <- levels(data)
+
+##data[1:10,]
+##rules <- apriori(data,parameter = list(minlen=3, supp=.9, conf=0.8))
+##,control = list(verbose=F),
+##		parameter = list(minlen=2, supp=.5, conf=0.8), 
+##		appearance = list(rhs=c("EatIt=edible","EatIt=poisonous"),
+##				default="lhs"))
+rules <- apriori(data,parameter = list(minlen=2, supp=.7, 
+				conf=0.8, target="rules"))
+		##,appearance =list(rhs=c("EatIt=edible","EatIt=poisonous"),default="lhs"))
 inspect(rules)
-#quality(rules) <- round(quality(rules), digits=3)
+##quality(rules) <- round(quality(rules), digits=3)
 
-#rules.sorted <- sort(rules, by="lift")
+##rules.sorted <- sort(rules, by="lift")
 
-#inspect(rules.sorted)
+##inspect(rules.sorted)
